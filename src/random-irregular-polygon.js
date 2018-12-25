c0af98a4caab6752365ddd6d8bc4352fdce52a95
Kelvin Au <kelvinau4413@hotmail.com>
2018-12-24 17:49:11 -0800

    sort points before returning

--- src/random-irregular-polygon.js
+++ src/random-irregular-polygon.js
@@ -13,8 +13,9 @@ class RIP {
     }
 
     getPolygonCoord() {
-
-        return this.space_partition(this.getRandomPoints());
+        const points = this.space_partition(this.getRandomPoints());
+        this.sortPoints(points);
+        return points;
     }
 
     getRandomPoints() {
@@ -118,4 +119,30 @@ class RIP {
             start.y + (end.y - start.y) * randomRatio
         );
     }
+
+  sortPoints(points) {
+      let p0 = {};
+      p0.y = Math.min.apply(null, points.map(p => p.y));
+      p0.x = Math.max.apply(null, points.filter( p=> p.y === p0.y).map(p => p.x));
+      points.sort((a,b) => angleCompare(p0, a, b));
+
+      function angleCompare(p0, a, b) {
+          const left = isLeft(p0, a, b);
+          if (left === 0) {
+            return distCompare(p0, a, b);
+          };
+          return left;
+      }
+
+      function isLeft(p0, a, b) {
+          return (a.x - p0.x) * (b.y - p0.y) - (b.x - p0.x) * (a.y - p0.y);
+      }
+
+      function distCompare(p0, a, b) {
+          const distA = (p0.x - a.x) * (p0.x - a.x) + (p0.y - a.y) * (p0.y - a.y);
+          const distB = (p0.x - b.x) * (p0.x - b.x) + (p0.y - b.y) * (p0.y - b.y);
+          return distA - distB;
+      }
+  };
+
 }
